4ed6413c36a1f52fd4fee2e0a598fec172f12046
Ayoub <aragrag.ayoub@gmail.com>
2023-10-29 00:14:47 -0300

    Edit main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,46 @@
+/*
+  Titre      : DEMO TMP36 - MKR 1000 - Thingsboard
+  Auteur     : Aragrag Ayoub
+  Date       : 28/10/2023
+  Description: lecture du TMP36 et envoie de cette valeur a une fréquences spécifique sur la plateforme IDO Thingsbaord
+  Droits     : Reproduction permise pour usage pédagogique
+  Version    : 0.0.4
+*/
+
 #include <SPI.h>
 #include <Arduino.h>
+#include <Adafruit_AHTX0.h>
+#include "WIFIConnector_MKR1000.h" //inclusion d'un libairie pour la focntionnalite wifi
+#include "MQTTConnector.h"         //inclusion d'un libairie pour la focntionnalite MQTT.h
 
-
-
+Adafruit_AHTX0 aht;
 const int del = 10000;
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
+    wifiConnect(); // Branchement au réseau WIFI
+    MQTTConnect(); // Branchement au broker MQTT
 
-    Serial.println("Adafruit AHT10/AHT20 demo");
+    Serial.println("Adafruit AHT10/AHT20 demo!");
+
+    if (!aht.begin())
+    {
+        Serial.println("Could not find AHT? Check wiring");
+        while (1)
+            delay(10);
+    }
     Serial.println("AHT10 or AHT20 found");
 }
 
-void loop() {
-    int temperature = random(10, 25);  // Remplacer par la lecture du capteur de température
-    int relative_humidity = random(20, 80);  // Remplacer par la lecture du capteur d'humidité
+void loop()
+{
+    sensors_event_t humidity, temp;
+    aht.getEvent(&humidity, &temp); 
 
-    // Envoyer les données via la console série
-    // Serial.print("T:");
-    Serial.print(temperature);
+    Serial.print(temp.temperature);
     Serial.print(",");
-    Serial.println(relative_humidity);
-
+    Serial.println(humidity.relative_humidity);
+    
     delay(del);
 }
